9d541beccf0ae9db44c8d1e085ca7df6eb5bad6b
José Fábio <jfsant4@gmail.com>
2022-07-11 22:12:04 -0300

    tela de pesquisa

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from ui_home import Ui_TelaHome
+from ui_pesquisa import Ui_TelaPesquisa
 from ui_principal import Ui_MainWindow
 
 
@@ -18,6 +19,26 @@ class TelaHome(QMainWindow, Ui_TelaHome):
         event.accept()
 
 
+class Pesquisa(QMainWindow, Ui_TelaPesquisa):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        self.setWindowTitle("Pesquisar")
+
+        self.telahome = TelaHome()
+
+        self.btn_homepesquisa.clicked.connect(self.open_home)
+
+    def open_home(self):
+        self.telahome.show()
+        self.hide()
+
+    def closeEvent(self, event) -> None:
+        self.origem = MainWindow()
+        self.origem.show()
+        event.accept()
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -25,11 +46,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Tela Principal")
 
         self.telahome = TelaHome()
+        self.telapesquisa = Pesquisa()
 
         self.btn_apenados.clicked.connect(self.open_home)
+        self.btn_pesquisa.clicked.connect(self.open_pesquisar)
 
     def open_home(self):
         self.telahome.show()
+        self.hide()
+
+    def open_pesquisar(self):
+        self.telapesquisa.show()
         self.hide()
 
 
