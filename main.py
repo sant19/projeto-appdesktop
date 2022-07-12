eb54f8159b8a19b8641caf5a53de6cc025066933
José Fábio <jfsant4@gmail.com>
2022-07-12 14:14:56 -0300

    Inseri tela relatório

--- main.py
+++ main.py
@@ -5,6 +5,34 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui_home import Ui_TelaHome
 from ui_pesquisa import Ui_TelaPesquisa
 from ui_principal import Ui_MainWindow
+from ui_relatorio import Ui_WindowRelatorio
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        self.setWindowTitle("Tela Principal")
+
+        self.telahome = TelaHome()
+        self.telapesquisa = Pesquisa()
+        self.telarelatorio = Relatorio()
+
+        self.btn_apenados.clicked.connect(self.open_home)
+        self.btn_pesquisa.clicked.connect(self.open_pesquisar)
+        self.btn_relatorio.clicked.connect(self.open_relatorio)
+
+    def open_home(self):
+        self.telahome.show()
+        self.hide()
+
+    def open_pesquisar(self):
+        self.telapesquisa.show()
+        self.hide()
+
+    def open_relatorio(self):
+        self.telarelatorio.show()
+        self.hide()
 
 
 class TelaHome(QMainWindow, Ui_TelaHome):
@@ -39,25 +67,11 @@ class Pesquisa(QMainWindow, Ui_TelaPesquisa):
         event.accept()
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class Relatorio(QMainWindow, Ui_WindowRelatorio):
     def __init__(self, parent=None):
         super().__init__(parent)
         super().setupUi(self)
-        self.setWindowTitle("Tela Principal")
-
-        self.telahome = TelaHome()
-        self.telapesquisa = Pesquisa()
-
-        self.btn_apenados.clicked.connect(self.open_home)
-        self.btn_pesquisa.clicked.connect(self.open_pesquisar)
-
-    def open_home(self):
-        self.telahome.show()
-        self.hide()
-
-    def open_pesquisar(self):
-        self.telapesquisa.show()
-        self.hide()
+        self.setWindowTitle("Relatório")
 
 
 if __name__ == "__main__":
